9b3cfaf3e3d15f30816c18ad9b605af742d648ba
Michael Hofweller <michael.hofweller@gmail.com>
2020-06-05 16:56:48 -0700

    image classification from live video

---
+++ sketch2.js
@@ -0,0 +1,41 @@
+/* eslint-disable */
+
+let mobileNet = ``;
+let video = ``;
+let label = ``;
+
+function modelReady() {
+        console.log('Model is ready!');
+        mobileNet.predict(gotResults);
+}
+
+function gotResults(error, results) {
+        if (error) {
+                console.error(error);
+        } else {
+            //    console.log(results); 
+                label = results[0].label;
+                mobileNet.predict(gotResults);
+                
+            }
+        }
+        
+        // function imageReady() {
+            //         image(dog, 0, 0, width, height);
+            // }
+            
+            function setup() {
+                createCanvas(600, 500);
+                video = createCapture(video);
+                mobileNet = ml5.imageClassifier('MobileNet', video, modelReady);
+                video.hide();
+                background(0);
+            }
+            
+            function draw(){
+                background(0);
+                image(video,0,0, width, 450)
+                fill(255);
+                textSize(15);
+                text(label, 10, height - 20);
+}
